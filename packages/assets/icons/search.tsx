37946742aeb6568caef1fb563ac5a29f375c887f
muhsiindeniiz <mnknsro413@gmail.com>
2025-11-18 22:27:58 +0300

    fix: icon props lint fix

--- packages/assets/icons/search.tsx
+++ packages/assets/icons/search.tsx
@@ -1,38 +1,38 @@
 import { forwardRef, SVGProps } from 'react'
 
-interface SearchIconProps extends SVGProps<SVGSVGElement> {
-  width?: number | string
-  height?: number | string
+interface SearchProps extends SVGProps<SVGSVGElement> {
+  className?: string
 }
 
-const SearchIcon = forwardRef<SVGSVGElement, SearchIconProps>(
-  ({ width, height, className, ...props }, ref) => {
-    return (
-      <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z"
-          stroke="#929EAE"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M18.9999 19L14.6499 14.65"
-          stroke="#929EAE"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    )
-  }
-)
+const Search = forwardRef<SVGSVGElement, SearchProps>(({ className, ...props }, ref) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      ref={ref}
+      {...props}
+    >
+      <path
+        d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.9999 19L14.6499 14.65"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+})
 
-SearchIcon.displayName = 'SearchIcon'
-export default SearchIcon
+Search.displayName = 'Search'
+export default Search
